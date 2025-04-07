f76b36fe25d9191ba1689a40406f06d03f1548b4
praveen9392 <bpraveenkumark1@gmail.com>
2025-04-07 23:03:21 +0530

    Completed Day 3 Assignment: Solved 2 Questions

--- day3-assignment.py
+++ day3-assignment.py
@@ -25,4 +25,4 @@ print(convert_list(nested_list))
 ### output= [[[[0, 1, 2], [3, 4, 5]], [[5, 6, 7], [9, 4, 2]]], [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [0, 1, 2]]]]
 
 
-### Hi, Sir Please let me know the inputs for my code. I am ready to take suggestions and improve further.
+### Hi Sir Please let me know the inputs for my code. I am ready to take suggestions and improve further.
