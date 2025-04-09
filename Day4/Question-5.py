2c1161e9d35dd0cd39838f8989916868986d668d
praveen9392 <bpraveenkumark1@gmail.com>
2025-04-09 22:29:22 +0530

    Completed Day 4 assignment: solved 2 questions

---
+++ Day4/Question-5.py
@@ -0,0 +1,18 @@
+class Poly:
+    def __init__(self, *cof):
+        self.cof = list(cof)
+
+    def __add__(self, b):
+        max_len = max(len(self.cof), len(b.cof))
+        self.cof = [0] * (max_len - len(self.cof)) + self.cof
+        b.cof = [0] * (max_len - len(b.cof)) + b.cof
+        final_result = [a + b for a, b in zip(self.cof, b.cof)]
+        return Poly(*final_result)
+
+    def __repr__(self):
+        return f"Poly({', '.join(map(str, self.cof))})"
+
+a = Poly(1, 2, 3)
+b = Poly(1, 0, 1, 1, 2, 3)
+c = a + b
+print(c)
---
+++ Day4/Question-6.py
@@ -0,0 +1,40 @@
+import os
+import datetime
+
+class File:
+    def __init__(self, path):
+        self.path = path
+
+    def getMaxSizeFile(self):
+        files = []
+        for root, _, filenames in os.walk(self.path):
+            for filename in filenames:
+                filepath = os.path.join(root, filename)
+                if os.path.isfile(filepath):
+                    size = os.path.getsize(filepath)
+                    files.append((filename, size))
+        
+        files.sort(key=lambda x: x[1], reverse=True)
+        return files[:2]
+
+    def getLatestFiles(self, date):
+        latest_files = []
+        for root, _, filenames in os.walk(self.path):
+            for filename in filenames:
+                filepath = os.path.join(root, filename)
+                if os.path.isfile(filepath):
+                    file_mod_time = datetime.date.fromtimestamp(os.path.getmtime(filepath))
+                    if file_mod_time > date:
+                        latest_files.append((filename, file_mod_time))
+
+        latest_files.sort(key=lambda x: x[1], reverse=True)
+        return latest_files
+
+
+if __name__ == '__main__':
+    path = r"C:\Users\Praveen\Desktop\handson\files"
+    fs = File(path)    
+    max_files = fs.getMaxSizeFile()
+    print("Largest files:", max_files)
+    latest_files = fs.getLatestFiles(datetime.date(2018, 2, 1))
+    print("Files modified after 1st Feb 2018:", latest_files)
